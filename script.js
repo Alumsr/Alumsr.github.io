b56442170c40b781b6b9ede21831cf4d4720965e
Alumsr <nbdio@yandex.com>
2023-11-07 18:53:57 +0800

    js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function home(){
-    var body = document.getElementById("main_body");
+    var body = document.getElementById("mybody");
     body.innerHTML = "<div class='note'>This is my home.</div>";
 }
 
 function notes(){
-    var notes = document.getElementById("main_body");
+    var notes = document.getElementById("mybody");
     notes.innerHTML = "<div class='note'>This is my notes.</div>";
 }
 
 function about(){
-    var about = document.getElementById("main_body");
+    var about = document.getElementById("mybody");
     about.innerHTML = "<div class='note'>About me.</div>";
 }
 
